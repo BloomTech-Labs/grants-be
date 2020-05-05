adc99d19caa9cdd9327d0011dc7b84b39e73c269
EvaGurtovaia <eva.gurtovaya@gmail.com>
2020-05-04 22:12:31 -0500

    Fix some stuff

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,7 @@
 module.exports = {
     development: {
         client: "pg",
-        connection: {
-            host: "127.0.0.1",
-            user: "node_user",
-            password: "Ng100852!",
-            database: "granted",
-            charset: "utf8",
-        },
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + "/knex/migrations",
         },
@@ -18,34 +12,27 @@ module.exports = {
         },
     },
     staging: {
-        client: "postgresql",
-        connection: {
-            database: "my_db",
-            user: "username",
-            password: "password",
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: __dirname + "/knex/migrations",
+        },
+        seeds: {
+            directory: __dirname + "/knex/seeds",
         },
         pool: {
             min: 2,
             max: 10,
-        },
-        migrations: {
-            tableName: "knex_migrations",
         },
     },
-
     production: {
-        client: "postgresql",
-        connection: {
-            database: "my_db",
-            user: "username",
-            password: "password",
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
+        client: "pg",
+        connection: process.env.DATABASE_URL,
         migrations: {
-            tableName: "knex_migrations",
+            directory: __dirname + "/knex/migrations",
+        },
+        seeds: {
+            directory: __dirname + "/knex/seeds",
         },
     },
 };
