b66d96f11be18e64ad9e604d34809142d54430a7
lee-wood <leemaewood@gmail.com>
2020-05-06 22:27:11 -0400

    added applicant user model functions

--- users/applicant_profiles/applicantProfileModel.js
+++ users/applicant_profiles/applicantProfileModel.js
@@ -1,1 +1,27 @@
 const db = require('../../knex/knex.js');
+
+//returns all applicant user type profiles
+function findApplicantProfiles() {
+  return db('applicant_profiles');
+}
+
+//returns specific applicant profile
+function findApplicantProfileById(id) {
+  return db('applicant_profiles')
+    .where({ id })
+    .first();
+}
+
+//updates data on applicant user profile
+function updateApplicantProfile(changes, id) {
+  return db('applicant_profiles')
+    .where({ id })
+    .first()
+    .update(changes);
+}
+
+module.exports = {
+  findApplicantProfiles,
+  findApplicantProfileById,
+  updateApplicantProfile
+}
