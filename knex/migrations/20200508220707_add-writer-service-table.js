0fdbabc1fd229efa52085dff54292146bb1ede57
lee-wood <leemaewood@gmail.com>
2020-05-08 22:15:16 -0400

    migration to add writer services table

---
+++ knex/migrations/20200508220707_add-writer-service-table.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('writer_services_offered', tbl => {
+    tbl.integer('writer_profile_id')
+      .unsigned()
+      .references('writer_profiles.id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    tbl.varchar('service_offered')
+      .notNullable();
+  });
+
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('writer_services_offered');
+};
