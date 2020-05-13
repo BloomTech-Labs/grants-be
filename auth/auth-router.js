e12c961d9de5eface6b083a7885deac0b59f08ea
Lee Wood <50416443+LeeWood@users.noreply.github.com>
2020-05-12 23:16:00 -0400

    Update auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -39,7 +39,7 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
     let { email, password } = req.body;
 
-    Users.findByEmail({ email })
+    Users.findByEmail( email )
         .first()
         .then((user) => {
             if (user && bcrypt.compareSync(password, user.password)) {
