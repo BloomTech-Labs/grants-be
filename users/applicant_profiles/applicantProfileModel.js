063afb62feefced54d8b64c76ec065320930427e
lee-wood <leemaewood@gmail.com>
2020-05-14 19:41:15 -0400

    creation of applicant user creates base profile

--- users/applicant_profiles/applicantProfileModel.js
+++ users/applicant_profiles/applicantProfileModel.js
@@ -19,6 +19,28 @@ function findApplicantProfileBy(filter) {
     .first();
 }
 
+//adds new applicant profile
+function addApplicantProfile() {
+
+  const defaultData = {
+    applicant_id: user.id,
+    first_name: " ",
+    last_name: " ",
+    city: " ",
+    state: " ",
+    country: " ",
+    zip: " ",
+    bio: "introduce yourself",
+    org_name: " ",
+    sector: "sector",
+    website_url: " "
+  }
+
+  db('applicant_profiles').insert(defaultData);
+
+  return("base user profile created")
+}
+
 //updates data on applicant user profile
 function updateApplicantProfile(changes, id) {
   return db('applicant_profiles')
@@ -31,5 +53,6 @@ module.exports = {
   findApplicantProfiles,
   findApplicantProfileById,
   findApplicantProfileBy,
+  addApplicantProfile,
   updateApplicantProfile
 }
