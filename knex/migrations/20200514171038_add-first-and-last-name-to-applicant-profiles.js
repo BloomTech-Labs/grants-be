37888c546963820237cd327194ba2c77989176e9
lee-wood <leemaewood@gmail.com>
2020-05-14 17:22:26 -0400

    updated applicant_user table with nee columns

---
+++ knex/migrations/20200514171038_add-first-and-last-name-to-applicant-profiles.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex) {
+  return knex.schema.table("applican_profiles", tbl => {
+    tbl.varchar("first_name");
+    tbl.varchar("last_name");
+    tbl.varchar("city");
+    tbl.varchar("state");
+    tbl.varchar("country");
+    tbl.varchar("zip");
+    tbl.dropColumn("contact_name");
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.table("applicant_profiles", tbl => {
+    tbl.varchar("contact_name");
+    tbl.dropColumn("zip");
+    tbl.dropColumn("country");
+    tbl.dropColumn("state");
+    tbl.dropColumn("city");
+    tbl.dropColumn("last_name");
+    tbl.dropColumn("first_name");
+  })
+};
