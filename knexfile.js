ed3d637f85c205a4ed5823993842afa2d0cabe7e
EvaGurtovaia <eva.gurtovaya@gmail.com>
2020-05-18 00:01:07 -0500

    Auth tests

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,16 @@ module.exports = {
             directory: __dirname + "/knex/seeds",
         },
     },
+    test: {
+        client: "pg",
+        connection: process.env.TEST_DATABASE_URL,
+        migrations: {
+            directory: __dirname + "/knex/migrations",
+        },
+        seeds: {
+            directory: __dirname + "/knex/seeds",
+        },
+    },
     staging: {
         client: "pg",
         connection: process.env.DATABASE_URL,
