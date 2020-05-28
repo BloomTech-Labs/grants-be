d725169433a0b0df8ed88fb37a0b25b9c5e2d50d
EvaGurtovaia <eva.gurtovaya@gmail.com>
2020-05-27 21:32:42 -0500

    Trying to find a bug with testing

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -1,44 +1,44 @@
-const supertest = require("supertest");
-const server = require("../api/server");
-const db = require("../knex/knex");
-const knexCleaner = require("knex-cleaner");
+// const supertest = require("supertest");
+// const server = require("../api/server");
+// const db = require("../knex/knex");
+// const knexCleaner = require("knex-cleaner");
 
-beforeAll(async () => {
-    // await db.migrate.latest();
-    await knexCleaner.clean(db, {
-        ignoreTables: ["knex_migrations", "knex_migrations_lock"],
-    });
-    // you can here also seed your tables, if you have any seeding files
-});
-afterAll(() => {
-    //   return db.migrate.rollback().then(() => db.destroy());
-    return db.destroy();
-});
+// beforeAll(async () => {
+//     // await db.migrate.latest();
+//     await knexCleaner.clean(db, {
+//         ignoreTables: ["knex_migrations", "knex_migrations_lock"],
+//     });
+//     // you can here also seed your tables, if you have any seeding files
+// });
+// afterAll(() => {
+//     //   return db.migrate.rollback().then(() => db.destroy());
+//     return db.destroy();
+// });
 
-test("create a user", async () => {
-    const res = await supertest(server).post("/api/auth/register").send({
-        email: "test@gmail.com",
-        password: "test",
-        user_type: "writer",
-    });
-    expect(res.status).toBe(201);
-    expect(res.type).toBe("application/json");
-    expect(res.body.email).toBe("test@gmail.com");
-});
+// test("create a user", async () => {
+//     const res = await supertest(server).post("/api/auth/register").send({
+//         email: "test@gmail.com",
+//         password: "test",
+//         user_type: "writer",
+//     });
+//     expect(res.status).toBe(201);
+//     expect(res.type).toBe("application/json");
+//     expect(res.body.email).toBe("test@gmail.com");
+// });
 
-test("login a user", async () => {
-    const res = await supertest(server)
-        .post("/api/auth/login")
-        .send({ email: "test@gmail.com", password: "test" });
-    expect(res.status).toBe(200);
-    expect(res.type).toBe("application/json");
-    expect(res.body.userType).toBe("writer");
-});
+// test("login a user", async () => {
+//     const res = await supertest(server)
+//         .post("/api/auth/login")
+//         .send({ email: "test@gmail.com", password: "test" });
+//     expect(res.status).toBe(200);
+//     expect(res.type).toBe("application/json");
+//     expect(res.body.userType).toBe("writer");
+// });
 
-test("try to login without email", async () => {
-    const res = await supertest(server)
-        .post("/api/auth/login")
-        .send({ email: "", password: "test" });
-    expect(res.status).toBe(401);
-});
+// test("try to login without email", async () => {
+//     const res = await supertest(server)
+//         .post("/api/auth/login")
+//         .send({ email: "", password: "test" });
+//     expect(res.status).toBe(401);
+// });
 
